1ea8e636f9c8cd34245f7d563a13f56873fa0bb1
Sebastian <sebastian.kuhlgert@gmail.com>
2018-06-06 11:34:31 -0400

    Updating compile script to get the latest github tag as a document version number

--- compile.sh
+++ compile.sh
@@ -8,16 +8,17 @@ echo "Compiling Help Files";
 node index.js create
 
 # Compiling the master markdown files from templates
-echo "Compile master files";
-node index.js compile -i help-master.md -o ./dist/PhotosynQ-Help-Manual.md -t 0.0.3
-node index.js compile -i tutorials-master.md -o ./dist/PhotosynQ-Getting-Started.md -t 0.0.3
-node index.js compile -i firmware-master.md -o ./dist/PhotosynQ-Firmware.md -t 0.0.3
+TAG=$(git describe --abbrev=0 --tags)
+echo "Compile master files for Github tag $TAG";
+node index.js compile -i help-master.md -o ./dist/PhotosynQ-Help-Manual.md -t $TAG
+node index.js compile -i tutorials-master.md -o ./dist/PhotosynQ-Getting-Started.md -t $TAG
+node index.js compile -i firmware-master.md -o ./dist/PhotosynQ-Firmware.md -t $TAG
 
 # Now we can build the pdfs as well
 echo "Converting markdown to pdf";
-node --no-deprecation index.js pdf -i ./dist/PhotosynQ-Help-Manual.md -o ./dist/PhotosynQ-Help-Manual.pdf
-node --no-deprecation index.js pdf -i ./dist/PhotosynQ-Getting-Started.md -o ./dist/PhotosynQ-Getting-Started.pdf
-node --no-deprecation index.js pdf -i ./dist/PhotosynQ-Firmware.md -o ./dist/PhotosynQ-Firmware.pdf
+node index.js pdf -i ./dist/PhotosynQ-Help-Manual.md -o ./dist/PhotosynQ-Help-Manual.pdf
+node index.js pdf -i ./dist/PhotosynQ-Getting-Started.md -o ./dist/PhotosynQ-Getting-Started.pdf
+node index.js pdf -i ./dist/PhotosynQ-Firmware.md -o ./dist/PhotosynQ-Firmware.pdf
 
 # Compiling finished
 echo -e "\033[92mDone...\033[0m";
